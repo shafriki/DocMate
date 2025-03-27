cc272c4a3cb846fd560c6ad157648ac8c00ee7e0
tanzid109 <tanzid.unkhalil109@gmail.com>
2025-03-27 18:57:51 +0600

    added service section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import BannerSection from "./components/BannerSection";
 import Updates from "./components/Updates";
 import About from "@/components/About";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <BannerSection />
       <Updates />
       <About/>
+      <Services/>
     </div>
   );
 }
