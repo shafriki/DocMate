f81b2887d86d36a9bc5918b2d9edce3ca4454f05
tanzid109 <tanzid.unkhalil109@gmail.com>
2025-03-27 14:58:57 +0600

    about section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import BannerSection from "./components/BannerSection";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <div>
       <BannerSection />
+      <About/>
     </div>
   );
 }
